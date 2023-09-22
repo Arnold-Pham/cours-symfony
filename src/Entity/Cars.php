c12f9eea4cffde6eb0f902eb0b727ec6695e19c8
Arnold Pham <arnold.pham@hotmail.com>
2023-09-22 10:25:33 +0200

    Symfony
    Pour qu'Hajar puisses suivre même en étant absente souvent
    - Après midi du 21/09/2023

---
+++ src/Entity/Cars.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CarsRepository::class)]
+class Cars
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $marque = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $modele = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMarque(): ?string
+    {
+        return $this->marque;
+    }
+
+    public function setMarque(string $marque): static
+    {
+        $this->marque = $marque;
+
+        return $this;
+    }
+
+    public function getModele(): ?string
+    {
+        return $this->modele;
+    }
+
+    public function setModele(string $modele): static
+    {
+        $this->modele = $modele;
+
+        return $this;
+    }
+}
